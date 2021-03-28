206bac4b096a77df422dd16a97da1420a9a80512
Tanya García <garcia.tanya0204@gmail.com>
2021-03-28 17:24:27 -0300

    Puntos 1 al 5

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  //Waits for the Observable to emit the array of heroes— which could happen now or several minutes from now
+  //The subscribe() method passes the emitted array to the callback, which sets the component's heroes property.
+  getHeroes():void{
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+  
+
+}
